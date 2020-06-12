ddd601f6e7dd30ba57a78960ede4b74e8a574105
Ben Oliver <benjamin.e.oliver@gmail.com>
2020-06-12 23:19:37 +0200

    updated init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,20 @@
+#!/bin/sh
+
+# make sure that you have set permissions for this file:
+# chmod 755 init.sh
+
+# to run this script use the following
+# source ./init.sh
+
+# use 'deactivate' to disable the environment
+
 if [ -d env ]; then 
     source env/bin/activate
 else
     python3 -m venv env
     source env/bin/activate
 fi
+
+if [ "$(pwd)/env/bin/python" = "$(which python)" ]; then 
+    echo "python environment set"
+fi 
