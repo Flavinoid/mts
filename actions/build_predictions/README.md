29d6447445d45f93af5bcd3452cd49bfabe70dd6
Ben Oliver <benjamin.e.oliver@gmail.com>
2020-06-13 15:07:00 +0200

    extract only Gene names

---
+++ actions/build_predictions/README.md
@@ -0,0 +1,5 @@
+# Mapping Gene predictions onto Proteins
+
+Having cleaned up the data from `TargetP` we now have a file called `mitochondrial_transfer_peptides.txt` that lists the names of every Genes that is predicted as being a "mitochondrial transfer peptide".
+
+Our goal now is to map this information onto Proteins.
--- src/simplify_targetP_results.py
+++ src/simplify_targetP_results.py
@@ -11,11 +11,17 @@ def collect_mitochondial_transfer_peptides(dir):
             asJson = json.load(file)
             sequences = asJson.get("SEQUENCES", [])
             for (gene, data) in sequences.items():
+                # exctract the 'actual' name that we want to use
+                # for example 'sp_Q15118_PDK1_HUMAN' maps to 'PDK1'
+                # the rule is <sp|tr>_<UniProtKB>_<GENE>_<HUMAN|Hm>(_<number>)
+                gene_name = gene.split("_")[2]
+                # we do this for all of them just in case the script changes at some point
+                # performance is not an issue!
                 prediction = data.get("Prediction", None)
                 if prediction == "Mitochondrial transfer peptide":
-                    matches.add(gene)
+                    matches.add(gene_name)
                 else:
-                    misses.add(gene)
+                    misses.add(gene_name)
     return (matches, misses)
 
 
