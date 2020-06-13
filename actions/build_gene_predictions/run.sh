76426a0f3f718b638b1466ae15f4ecebb9c7a61f
Ben Oliver <benjamin.e.oliver@gmail.com>
2020-06-13 16:39:09 +0200

    write prediction file

--- actions/build_gene_predictions/run.sh
+++ actions/build_gene_predictions/run.sh
@@ -2,5 +2,6 @@
 GENE_AND_ASSOCIATED_PROTEINS="data/in/gene_and_associated_proteins.csv"
 MITOCHONDRIAL_TRANSFER_PEPTIDES="../upload_fasta_files/data/out/mitochondrial_transfer_peptides.txt"
 SIGNAL_PEPTIDES="../upload_fasta_files/data/out/signal_peptides.txt"
+OUT_DIR="data/out"
 
-python ../../src/gene_predictions.py "${GENE_AND_ASSOCIATED_PROTEINS}" "${MITOCHONDRIAL_TRANSFER_PEPTIDES}" "${SIGNAL_PEPTIDES}"
+python ../../src/gene_predictions.py "${GENE_AND_ASSOCIATED_PROTEINS}" "${MITOCHONDRIAL_TRANSFER_PEPTIDES}" "${SIGNAL_PEPTIDES}" "${OUT_DIR}"
--- src/gene_predictions.py
+++ src/gene_predictions.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 if __name__ == "__main__":
@@ -5,6 +6,7 @@ if __name__ == "__main__":
     gene_and_associated_proteins_path = sys.argv[1]
     mitochondrial_transfer_peptide_proteins_path = sys.argv[2]
     signal_peptides_path = sys.argv[3]
+    output_dir = sys.argv[4]
 
     with open(
         mitochondrial_transfer_peptide_proteins_path, "r"
@@ -17,6 +19,7 @@ if __name__ == "__main__":
         signal_matches = set(map(lambda x: x.strip(), signal_matches_file.readlines()))
 
     matches = transfer_matches.union(signal_matches)
+    genes = set()
 
     with open(gene_and_associated_proteins_path, "r") as f:
         data = map(lambda x: x.strip(), f.readlines())
@@ -26,4 +29,8 @@ if __name__ == "__main__":
             gene = gene_str.split(";")[0]
             protein_set = set(protein_str.split(";"))
             if not protein_set.isdisjoint(matches):
-                print(gene)
+                genes.add(gene)
+
+    with open(os.path.join(output_dir, "predicted.txt"), "w") as f:
+        for gene in genes:
+            f.write("{}\n".format(gene))
